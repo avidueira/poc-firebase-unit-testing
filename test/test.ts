3691e7cb6d29055456228e001f4d8046b7f9bc58
Alfredo Vidueira <avidueira@kunder.cl>
2020-06-28 02:37:01 -0400

    test 1

--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,6 @@ import assert from 'assert';
 
 describe('POC Firebase Unit Testing App', () => {
     it('Understands basic addition', () => {
-        assert.strictEqual(2 + 2, 4);
+        assert.strictEqual(2 + 3, 5);
     });
 });
